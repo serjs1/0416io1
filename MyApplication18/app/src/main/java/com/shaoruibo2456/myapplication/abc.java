91c4fa49d4ad0ec8a71370a07b03c049d01a0977
shaoruibo <1119637389@qq.com>
2020-04-14 16:11:34 +0800

    第一次传输

---
+++ MyApplication18/app/src/main/java/com/shaoruibo2456/myapplication/abc.java
@@ -0,0 +1,7 @@
+package com.shaoruibo2456.myapplication;
+
+public class abc {
+    public static void main(String[] args) {
+        System.out.println("nao tan");
+    }
+}
---
+++ MyApplication18/app/src/main/java/com/shaoruibo2456/myapplication/abcd.java
@@ -0,0 +1,7 @@
+package com.shaoruibo2456.myapplication;
+
+public class abcd {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
